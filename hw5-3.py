aef19a34e8d1ac58d83acfbb0339795286d6162f
p22jiaropoli <p22jiaropoli@fairfieldprep.org>
2022-01-14 10:03:59 -0500

    Add file

---
+++ hw5-3.py
@@ -0,0 +1,16 @@
+# Author JRI 11/2/21
+
+import calendar
+
+# Question 1
+calendar.TextCalendar().pryear(2020)
+
+# Question 2
+calendar.textcalendar(6).pryear(2021)
+
+# Question 3
+calendar.textcalendar.prmonth(2021, 1)
+
+# Question 5
+print(calendar.isleap(2020))
+print(calendar.isleap(2021))
